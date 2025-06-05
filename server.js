3eef6f35e9c9059e14fac76cc13850a665ddcf71
amanraula <amanraula2005@gmail.com>
2025-06-06 02:01:09 +0530

    MongoDb And Authentication

--- server.js
+++ server.js
@@ -4,20 +4,28 @@ import { ENV_VARS } from "./config/envVars.js";
 import gemRoutes from "./gemini/gemroutes.js"
 import weatherRoutes from "./weather/weatherroutes.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./route/auth.route.js";
 
+import { connectDB } from "./config/db.js";
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // will allow us to parse req.body
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.use(cors());
 // app.use("/", (req, res) => {
 //   res.send("Hello from server");
 // }
 // );
+app.use("/api/v1/auth", authRoutes);
 app.use("/plan", planroute);
 app.use("/gemini", gemRoutes);
 app.use("/weather", weatherRoutes);
-app.listen(PORT);
+
+app.listen(PORT, () => {
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
+});
 console.log("server started at port", PORT);
---
+++ utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import { ENV_VARS } from "../config/envVars.js";
+
+export const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, "my_secret_token", { expiresIn: "15d" });
+
+	res.cookie("omtours-jwt", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not be accessed by JS
+	//	sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		sameSite: "lax", // CSRF attacks cross-site request forgery attacks
+		secure: false,
+	});
+
+	return token;
+};
