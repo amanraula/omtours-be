2e6e649a069c92fad17cde6eb413d5b93d5d2b2d
amanraula <amanraula2005@gmail.com>
2025-06-06 22:04:54 +0530

    sameSite

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not be accessed by JS
 	//	sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		sameSite: "lax", // CSRF attacks cross-site request forgery attacks
+		sameSite: "none", // CSRF attacks cross-site request forgery attacks
 		secure: false,
 	});
 
