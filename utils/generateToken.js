41689fa86178410d716df4591408871d40ddbbe9
amanraula <amanraula2005@gmail.com>
2025-06-06 21:38:45 +0530

    keyToken

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, "my_secret_token", { expiresIn: "15d" });
+	const token = jwt.sign({ userId }, ENV_VARS.JWT_TOKEN, { expiresIn: "15d" });
 
 	res.cookie("omtours-jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
